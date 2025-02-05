98835980925c20a850f14897054b5a341aae398d
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2025-02-05 23:33:01 +0100

    feat: App layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="flex flex-col h-full justify-center gap-4">
+      <h1 className="text-center font-bold text-2xl">Transfer Files</h1>
+      <div className="outline-dashed outline-2 rounded-2xl max-h-96 p-2 flex-1">Test</div>
+      <button className="">Send</button>
+    </div>
   );
 }
