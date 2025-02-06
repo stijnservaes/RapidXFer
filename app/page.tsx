47cf2c2d56f05d98817eca58381401e2610c85f8
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2025-02-06 20:11:48 +0100

    feat: Button styling

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setSenderMail(e.currentTarget.value)
           }
-          className="text-black dark:text-white placeholder-gray-600 dark:placeholder-white dark:bg-gray-600 py-1 px-4 rounded-full focus:outline focus:outline-2 dark:outline-white" 
+          className="rounded-full px-4 py-1 text-black placeholder-gray-600 focus:outline focus:outline-2 dark:bg-gray-600 dark:text-white dark:placeholder-white dark:outline-white"
         />
         <input
           type="email"
@@ -33,10 +33,12 @@ export default function Home() {
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setReceiverMail(e.currentTarget.value)
           }
-          className="text-black dark:text-white placeholder-gray-600 dark:placeholder-white dark:bg-gray-600 py-1 px-4 rounded-full focus:outline focus:outline-2 dark:outline-white" 
+          className="rounded-full px-4 py-1 text-black placeholder-gray-600 focus:outline focus:outline-2 dark:bg-gray-600 dark:text-white dark:placeholder-white dark:outline-white"
         />
         <Filedrag step={step} setStep={setStep} file={file} setFile={setFile} />
-        <button>Send</button>
+        <button className="self-center w-1/4 font-bold rounded-full bg-slate-800 px-4 py-2 text-white hover:bg-zinc-400 dark:hover:bg-zinc-800 hover:text-slate-800 dark:hover:text-zinc-400 dark:hover:outline-zinc-400 hover:outline hover:outline-slate-800 dark:bg-slate-500 dark:text-white">
+          Send
+        </button>
       </div>
     </main>
   );
