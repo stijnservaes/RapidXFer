7a1b70c90c98286332fa0fa89b8f17832d65f28e
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2025-02-08 19:24:22 +0100

    feat: Zod parsing

--- app/actions.ts
+++ app/actions.ts
@@ -2,8 +2,33 @@
 
 import { supabase } from "@/lib/supabase"
 import { randomUUID } from "crypto"
+import { z } from "zod"
+
+const uploadMetadataSchema = z.object({
+  senderMail: z.string().email("Invalid email adress."),
+  receiverMail: z.string().email("Invalid email address."),
+  fileName: z.string().min(1, "File name is required."),
+  fileType: z.string().min(1, "File type is reaquired")
+})
 
 export async function uploadMetadata(formData: FormData) {
+  const data = {
+    senderMail: formData.get("senderMail"),
+    receiverMail: formData.get("receiverMail"),
+    fileName: formData.get("fileName"),
+    fileType: formData.get("fileType")
+  }
+
+  const parsedData = uploadMetadataSchema.safeParse(data)
+
+  if (!parsedData.success) {
+    return {
+      success: false, 
+      message: parsedData.error.errors.join(" ")
+    }
+  }
+
+  
 
   return {
     success: true,
