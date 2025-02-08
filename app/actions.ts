68068954099e0c3e50ea18fb0973cc22c985580e
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2025-02-08 21:10:18 +0100

    feat: Prisma metadata table integration

--- app/actions.ts
+++ app/actions.ts
@@ -1,47 +1,72 @@
-"use server"
+"use server";
 
-import { supabase } from "@/lib/supabase"
-import { z } from "zod"
+import { supabase } from "@/lib/supabase";
+import { z } from "zod";
+import { randomUUID } from "crypto";
+import { prisma } from "@/lib/prisma";
 
 const uploadMetadataSchema = z.object({
   senderMail: z.string().email("Invalid email adress."),
   receiverMail: z.string().email("Invalid email address."),
   fileName: z.string().min(1, "File name is required."),
-  fileType: z.string().min(1, "File type is reaquired")
-})
+  fileType: z.string().min(1, "File type is reaquired"),
+});
 
 export async function uploadMetadata(formData: FormData) {
   const data = {
     senderMail: formData.get("senderMail"),
     receiverMail: formData.get("receiverMail"),
     fileName: formData.get("fileName"),
-    fileType: formData.get("fileType")
-  }
+    fileType: formData.get("fileType"),
+  };
 
-  const parsedData = uploadMetadataSchema.safeParse(data)
-
+  const parsedData = uploadMetadataSchema.safeParse(data);
   if (!parsedData.success) {
     return {
-      success: false, 
-      message: parsedData.error.errors.join(" ")
-    }
-  }
-  
-  const {data: signedData, error: signedError} = await supabase.storage.from("uploadedfiles").createSignedUploadUrl(parsedData.data.fileName)
-  if (signedError) {
-    return {
       success: false,
-      message: signedError.message
+      message: parsedData.error.errors.join(" "),
+    };
+  }
+
+  try {
+    const fileName = `${randomUUID()}-${parsedData.data.fileName}`;
+    const { data: signedData, error: signedError } = await supabase.storage
+      .from("uploadedfiles")
+      .createSignedUploadUrl(fileName);
+    if (signedError) {
+      return {
+        success: false,
+        message: signedError.message,
+      };
     }
-  }
 
-  return {
-    success: true,
-    message: signedData.signedUrl
-  }
+    await prisma.metadata.create({
+      data: {
+        senderMail: parsedData.data.senderMail,
+        receiverMail: parsedData.data.receiverMail,
+        fileName: fileName,
+        fileType: parsedData.data.fileType,
+        signedUrl: signedData.signedUrl,
+      },
+    });
 
-  return {
-    success: true,
-    message: ""
+    return {
+      success: true,
+      message: signedData.signedUrl,
+    };
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+      return {
+        success: false,
+        message: "Database Error occurred.",
+      };
+    } else {
+      console.log("An unknown error occurred.");
+      return {
+        success: false,
+        message: "An unknown error occurred.",
+      };
+    }
   }
 }
